51a26ae8e65315c6abc08ea67e33f77aabb3bb9c
Max <zl2480@columbia.edu>
2019-11-02 15:50:46 +0800

    Implement the ssq class

--- shuangseqiu.py
+++ shuangseqiu.py
@@ -1,4 +1,7 @@
 from collections import Counter
+import random
+import pickle as pkl
+import datetime
 
 from playground.simulator import Simulator
 from playground.state import State
@@ -9,21 +12,89 @@ from playground.strategy import Strategy
 
 class SSQ(State):
 
-	def __init__(self, date, red, blue):
+	def __init__(self,
+				date, 
+				red_balls,
+				blue_ball,
+				sales=None,
+				pool_size=None,
+				price_count=None,
+				price_money=None
+				):
 		self.date = date
-		self.red = red
-		self.blue = blue
+		self.red_balls = red_balls
+		self.blue_ball = blue_ball
+		self.pool_size = pool_size
+		self.sales = sales
+		self.price_count = price_count
+		self.price_money = price_money
+
+	@classmethod
+	def from_prediction(cls, date, red_balls, blue_ball):
+		return cls(date=date, red_balls=red_balls, blue_ball=blue_ball)
+
+	@classmethod
+	def random(cls, date, seed=None):
+		random.seed(seed)
+		red_balls = random.sample(range(1, 34), k=6)
+		blue_ball = random.choice(range(1,  17))
+		return cls.from_prediction(date, red_balls, blue_ball)
 
 	def check_result(self, other):
 		if self.date != other.date:
 			raise ValueError('Not from same date')
 
-		if sorted(self.red) == sorted(other.red):
-			return 100
-		elif self.blue == other.blue:
-			return 1
-		else:
-			return 0
+		n_match_red = self.number_of_matches(self.red_balls, other.red_balls)
+		is_match_blue = (self.blue_ball == other.blue_ball)
+
+		if n_match_red == 6 and is_match_blue:
+			# 一等奖
+			return self.price_money[1]
+		elif n_match_red == 6 and not is_match_blue:
+			# 二等奖
+			return self.price_money[2]
+		elif n_match_red == 5 and is_match_blue:
+			# 三等奖
+			return self.price_money[3]
+		elif (n_match_red == 5 and not is_match_blue) or (n_match_red == 4 and is_match_blue):
+			# 四等奖
+			return self.price_money[4]
+		elif (n_match_red == 4 and not is_match_blue) or (n_match_red == 3 and is_match_blue):
+			# 五等奖
+			return self.price_money[5]
+		elif n_match_red in (0, 1, 2) and is_match_blue:
+			# 六等奖
+			return self.price_money[6]	
+
+	def number_of_matches(self, a, b):
+		return len(set(a) & set(b))
+
+	def __repr__(self):
+		return '<date: {} red: {} blue: {}>'.format(self.date, self.red_balls, self.blue_ball)
+
+
+def load_history(data_path=None, start_date=None, end_date=None):
+	""" Get the actual ticket result between start date and end date (inclusive), 
+		start_date and end_date can be datetime object of string of format 'yyyy-mm-dd'
+
+		ex. load_history(start_date='2019-09-01', end_date='2019-10-01')  # 12 records
+	"""
+	start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date() if isinstance(start_date, str) else start_date
+	end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d').date() if isinstance(end_date, str) else end_date
+
+	data_path = data_path or './data/shuangseqiu.pkl'
+	with open(data_path, 'rb') as f:
+		history = pkl.load(f)
+
+	history_states = [SSQ(**i) for i in history \
+						if (start_date is None or start_date <= i['date']) and (end_date is None or end_date >= i['date'])]
+	return history_states
+
+
+
+#########################################
+# Implementing some basic strategy ######
+#########################################
 
 
 class FrequencyBasedStrategy(Strategy):
@@ -40,3 +111,4 @@ class FrequencyBasedStrategy(Strategy):
 		prev_states = prev_states[-self.lookback_period:]
 
 		
+
--- utils.py
+++ utils.py
@@ -7,35 +7,47 @@ import pickle as pkl
 import os
 
 
-def fetch_shuangseqiu_result(url):
+def fetch_shuangseqiu_result(url, use_default_encoding=False):
 	page = requests.get(url)
 	assert page.status_code == 200
 
-	page_content = page.content.decode('gbk')
+	if use_default_encoding:
+		page_content = page.content.decode(page.apparent_encoding)
+	else:
+		page_content = page.content.decode('gbk')
+		
 	soup = BeautifulSoup(page_content, 'lxml')
 
-	date = soup.find('span', attrs={'class': 'span_right'}).text[:18]
-	year, month, day = re.findall('[0-9]+', date)
+	date = soup.find('span', attrs={'class': 'span_right'}).text
+	year, month, day, *_ = re.findall('[0-9]+', date)
 	date = datetime.date(int(year), int(month), int(day))
 
 	red_balls = [int(e.text.strip()) for e in soup.find_all('li', class_='ball_red')]
 	blue_ball = int(soup.find('li', class_='ball_blue').text.strip())
 
-	sales, pool_size = re.findall('[0-9,]+元', page_content)
-	sales = float(sales[:-1].replace(',', ''))
-	pool_size = float(pool_size[:-1].replace(',', ''))
+	try:
+		sales, pool_size = re.findall('[0-9,-]+元', page_content)
+		sales = float(sales[:-1].replace(',', '')) if '-' not in sales else None
+		pool_size = float(pool_size[:-1].replace(',', '')) if '-' not in pool_size else None
+	except:
+		sales, pool_size = None, None
 
 	price = soup.find_all('table', class_='kj_tablelist02')[1]
 	# read_html doesn't work with bs4.Tag
 	price: pd.DataFrame = pd.read_html(str(price))[0]
 
 	price_money, price_count = {}, {}
-	for _, row in price.iterrows():
-		if row[0].endswith('等奖'):
-			price_count[row[0]] = 0 if '--' in row[1] else float(row[1])
-			price_money[row[0]] = 5000000 if '--' in row[2] else float(row[2])
+	for row_id, row in price.iterrows():
+		# if row[0].endswith('等奖') and not row[0].startswith('幸运'):
+		# 	price_count[row[0]] = 0 if '--' in row[1] else float(row[1])
+		# 	price_money[row[0]] = 5000000 if '--' in row[2] else float(row[2])
 
-	return {'date': date, 'red_balls': red_balls, 'blue_ball': blue_ball, 'pool_size': pool_size,
+		if 2 <= row_id <= 7:
+			price_count[row_id-1] = 0 if '--' in row[1] else float(row[1])
+			price_money[row_id-1] = 5000000 if '--' in row[2] else float(row[2])
+
+	return {'date': date, 'red_balls': red_balls, 'blue_ball': blue_ball, 
+			'sales': sales, 'pool_size': pool_size,
 			'price_count': price_count, 'price_money': price_money}
 
 
@@ -64,7 +76,13 @@ def download_shuangseqiu(save_dir=None):
 
 	res = []
 	for url in tqdm(all_urls):
-		res.append(fetch_shuangseqiu_result(url))
+		try:
+			res.append(fetch_shuangseqiu_result(url, use_default_encoding=False))
+		except Exception as e:
+			try:
+				res.append(fetch_shuangseqiu_result(url, use_default_encoding=True))
+			except:
+				print('Failed: {}'.format(url))
 
 	with open(save_path, 'wb') as f:
 		pkl.dump(res, f)
@@ -82,3 +100,4 @@ if __name__ == '__main__':
 
 	if args.which.lower() == 'shuangseqiu':
 		download_shuangseqiu()
+	# print(fetch_shuangseqiu_result('http://kaijiang.500.com/shtml/ssq/19124.shtml', use_default_encoding=False))
